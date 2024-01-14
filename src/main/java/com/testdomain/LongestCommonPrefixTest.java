ad300131b561ce84170cfbebd389b28318739374
Egor93 <egor93gordeev@gmail.com>
2024-01-14 19:47:37 +0100

    Prefix viable solution, probably ineffective

--- src/main/java/com/testdomain/LongestCommonPrefixTest.java
+++ src/main/java/com/testdomain/LongestCommonPrefixTest.java
@@ -27,12 +27,36 @@ public class LongestCommonPrefixTest {
     @Test
     public void PrefixTest() {
         Assert.assertEquals(
-                LongestCommonPrefix(new String[]{"flower", "flow", "flight"}), "fl");
+                LongestCommonPrefix(new String[]{"flower", "flow", "floght"}), "flo");
         Assert.assertEquals(
-                LongestCommonPrefix(new String[]{"dog","racecar","car"}),"");
+                LongestCommonPrefix(new String[]{"dog", "racecar", "car"}), "");
     }
 
     private String LongestCommonPrefix(String[] strings) {
+        String prefix = "";
+        //iterate over all words in array
+        for (int charIndex = 0; charIndex < 200; charIndex++) {
+            String prefixTemp = ""+strings[0].charAt(charIndex);
+            System.out.println(prefix);
+            for (int wordIndex = 1;wordIndex<strings.length;wordIndex++){
+                try {
+                    char NthChar = strings[wordIndex].charAt(charIndex);
+                    char NthCharPrev = strings[wordIndex-1].charAt(charIndex);
+                    if (NthChar!=NthCharPrev){
+                        return prefix;
+                    }
+                    System.out.println(NthChar);
+                } catch (Exception e){
+                    System.out.println("Exception"+e);
+                    return prefix;
+                }
+            }
+            prefix+=prefixTemp;
+        }
+        //in each array iterate over characters
+        //set tempPrefix to the array[0][0]
+        //if tempPrefix is constant append it to the prefix
+        //if not return
         return "";
     }
 }
